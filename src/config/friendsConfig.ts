43540902f02b83563cc33f18265f4bf7ce2524c2
Vio01Kaki3 <2565047411@qq.com>
2026-06-07 18:02:14 +0800

    导航栏美化

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -23,12 +23,12 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
-		title: "夏夜流萤",
+		title: "Vio01_Kaki3",
 		imgurl:
-			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-		siteurl: "https://blog.cuteleaf.cn",
-		tags: ["Blog"],
+			"file:///C:/Users/25650/Pictures/21312.jpg",
+		desc: "每天都可以吃到美食！",
+		siteurl: "https://blog.qsanj.com",
+		tags: ["Blog"],	
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -30,16 +30,6 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		],
 	});
 
-	// 根据配置决定是否添加友链，在siteConfig关闭pages.friends时导航栏不显示友链
-	if (siteConfig.pages.friends) {
-		links.push(LinkPreset.Friends);
-	}
-
-	// 根据配置决定是否添加留言板，在siteConfig关闭pages.guestbook时导航栏不显示留言板
-	if (siteConfig.pages.guestbook) {
-		links.push(LinkPreset.Guestbook);
-	}
-
 	// 我的及其子菜单
 	links.push({
 		name: "我的",
@@ -60,45 +50,18 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		url: "/content/",
 		icon: "material-symbols:info",
 		children: [
-			// 根据配置决定是否添加赞助，在siteConfig关闭pages.sponsor时导航栏不显示赞助
-			...(siteConfig.pages.sponsor ? [LinkPreset.Sponsor] : []),
-
 			// 关于页面
 			LinkPreset.About,
 			// 根据配置决定是否添加留言板，在siteConfig关闭pages.guestbook时导航栏不显示留言板
 			...(siteConfig.pages.guestbook ? [LinkPreset.Guestbook] : []),
-		],
-	});
-
-	// 自定义导航栏链接,并且支持多级菜单
-	links.push({
-		name: "链接",
-		url: "/links/",
-		icon: "material-symbols:link",
-
-		// 子菜单
-		children: [
+			// 友情链接页面
 			{
-				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:github",
-			},
-			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
+				name: "朋友们",
+				url: "/friends/",
 			},
 		],
 	});
-
+	
 	// 仅返回链接，其它导航搜索相关配置在模块顶层常量中独立导出
 	return { links } as NavBarConfig;
 };
